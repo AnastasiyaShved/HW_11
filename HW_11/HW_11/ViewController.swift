9be13d4b008ebd934385ba909a8b0cf0dff45d03
shvedpavel <shvedpavel25@gmail.com>
2023-08-15 01:07:08 +0300

    fix after review

--- HW_11/HW_11/ViewController.swift
+++ HW_11/HW_11/ViewController.swift
@@ -9,35 +9,31 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var progressView: UIProgressView!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var buttonSetTime: UIButton!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var `switch`: UISwitch!
-    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet private weak var progressView: UIProgressView!
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var datePicker: UIDatePicker!
+    @IBOutlet private weak var buttonSetTime: UIButton!
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var `switch`: UISwitch!
+    @IBOutlet private weak var buttonClear: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-        
     }
 
-    @IBAction func slider(_ sender: UISlider) {
+    @IBAction private func slider(_ sender: UISlider) {
         textField.text = String(round(sender.value*100)/100)
         progressView.progress = sender.value
     }
     
-    @IBAction func changeTextV(_ sender: UITextField) {
+    @IBAction private func changeTextV(_ sender: UITextField) {
         slider.setValue(Float(String(sender.text ?? "")) ?? 0, animated: true)
         progressView.progress = Float(String(sender.text ?? "")) ?? 0
     }
     
-    @IBAction func buttonSetTime(_ sender: UIButton) {
+    @IBAction private func buttonSetTime(_ sender: UIButton) {
         let  timeFormater = DateFormatter()
         timeFormater.dateFormat = "HH:mm"
         
@@ -45,11 +41,11 @@ final class ViewController: UIViewController {
         label.text = time
     }
     
-    @IBAction func `switch`(_ sender: UISwitch) {
+    @IBAction private func `switch`(_ sender: UISwitch) {
         label.backgroundColor = sender.isOn ? .blue : .gray
     }
     
-    @IBAction func buttonClear(_ sender: UIButton) {
+    @IBAction private func buttonClear(_ sender: UIButton) {
         label.text = ""
         `switch`.setOn(false, animated: true)
         label.backgroundColor = .gray
